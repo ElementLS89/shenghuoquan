430643017105ce39d7e057e65ce1621eb8b0c80a
ElementLS <lishuai891026@163.com>
2018-08-12 23:18:00 +0800

    test clint

--- server/config.js
+++ server/config.js
@@ -35,7 +35,7 @@ const CONF = {
          * 新加坡：sg
          * @see https://www.qcloud.com/document/product/436/6224
          */
-        region: 'ap-south',
+        region: 'ap-guangzhou',
         // Bucket 名称
         fileBucket: 'qcloudtest',
         // 文件夹
